1fa4c337f9d2cc87f1e942df72b4a1bb4d9fb7bc
@veretenkin <veretenkin@rozetka.com.ua>
2021-12-22 09:58:07 +0200

    add openapi

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
   };
   app.enableCors(options);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+  .setTitle('Api endpoints')
+  .setDescription('The iot API description')
+  .setVersion('1.0')
+  .addTag('iot')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 
